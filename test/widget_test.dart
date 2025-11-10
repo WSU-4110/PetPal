e0f059e0fee3cae52a3ba229e219562a398e3763
Maryam Kassem <maryamkassem@Maryams-MacBook-Air-2.local>
2025-11-10 01:41:53 -0500

    trying for the hunderedthhhhh time to upload my assignment 5 test

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('dummy widget test passes', (WidgetTester tester) async {
+    // This is just a placeholder widget test so that Assignment 5 runs cleanly.
+    final sum = 1 + 1;
+    expect(sum, 2);
+  });
+}
